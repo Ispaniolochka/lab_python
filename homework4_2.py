b0c35fd4125d4322f0515960cc1d12b0f8eca2a9
Ispaniolochka <olashmaker@rambler.ru>
2018-11-22 22:11:49 +0300

    готово

--- homework4_2.py
+++ homework4_2.py
@@ -1,13 +1,14 @@
 s = "У лукоморья 123 дуб зеленый 456"
 print('1. Буква "я" встречается в строке:',s.count('я'))
-print('2. Буква "у" встречается в строке:',s.count('у')
-    if s.isalpha():
-        print("В строке есть цифры.")
-    else:
-        print('3.',s.upper()
-x = s.(len)
-def length(x):
-    if x > 4:
-        print(s.lower('У лукоморья 123 дуб зеленый 456'))
+print('2. Буква "у" встречается в строке:',s.count('у'))
+if s.isalpha():
+    print("В строке есть цифры.")
+else:
+    print('3.',s.upper())
+x = len(s)
+if x > 4:
+    print('4.',s.lower())
           
 print('5.',s.replace('У','O'))
+
+              
